94b90b7b270d7d426259614747033456ef0a38b2
Tatsuhiro Terada <tatsuhirotera@gmail.com>
2024-03-23 22:35:02 +0900

    Updates

---
+++ precache-manifest.44954438b3677ebeae406b1185c71a4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517dd5e7da56e0d9078b6a188d854b4f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "6a9c9267f84aac11bfea",
+    "url": "/react-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8087fcb5eccca3a52b2c",
+    "url": "/react-portfolio/static/css/main.8f43d6b9.chunk.css"
+  },
+  {
+    "revision": "6a9c9267f84aac11bfea",
+    "url": "/react-portfolio/static/js/2.779302e9.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-portfolio/static/js/2.779302e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8087fcb5eccca3a52b2c",
+    "url": "/react-portfolio/static/js/main.42eed55f.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
