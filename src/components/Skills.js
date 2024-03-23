7eb32109c43602ef4ccd55f3e32153f795c65e9c
Tatsuhiro Terada <tatsuhirotera@gmail.com>
2024-03-23 22:28:10 +0900

    Reverted

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,30 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Skills extends Component {
-  renderSkillsByCategory(category) {
-    // Filter skills by category and map to list items
-    return this.props.sharedSkills.icons
-      .filter(skill => skill.category === category)
-      .map((skill, i) => (
-        <li className="list-inline-item mx-3" key={i}>
-          <span>
-            <div className="text-center skills-tile">
-              <i className={skill.class} style={{ fontSize: '220%' }}>
-                <p className="text-center" style={{ fontSize: '30%', marginTop: '4px' }}>
-                  {skill.name}
-                </p>
-              </i>
-            </div>
-          </span>
-        </li>
-      ));
+  renderSkillIcons(skills) {
+    return skills.map((skill, i) => (
+      <li className="list-inline-item mx-3" key={i}>
+        <span>
+          <div className="text-center skills-tile">
+            <i className={skill.class} style={{ fontSize: "220%" }}>
+              <p className="text-center" style={{ fontSize: "30%", marginTop: "4px" }}>
+                {skill.name}
+              </p>
+            </i>
+          </div>
+        </span>
+      </li>
+    ));
   }
 
   render() {
+    let technicalSkills = [];
+    let programmingSkills = [];
     if (this.props.sharedSkills && this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.skills;
-      var knowledge = this.renderSkillsByCategory('knowledge');
-      var programming = this.renderSkillsByCategory('programming');
+      if (this.props.sharedSkills.technical && this.props.sharedSkills.technical.icons) {
+        technicalSkills = this.renderSkillIcons(this.props.sharedSkills.technical.icons);
+      }
+      if (this.props.sharedSkills.programming && this.props.sharedSkills.programming.icons) {
+        programmingSkills = this.renderSkillIcons(this.props.sharedSkills.programming.icons);
+      }
     }
 
     return (
@@ -35,16 +38,15 @@ class Skills extends Component {
               <span className="text-white">{sectionName}</span>
             </h1>
           </div>
-          <div className="col-md-12 text-center">     
-              <h2 className="subsection-title">
-                <span className="text-white">Knowledge</span>
-              </h2>
-              <ul className="list-inline mx-auto skill-icon">{knowledge}</ul>
-
-              <h2 className="subsection-title">
-                <span className="text-white">Programming Skills</span>
-              </h2>
-              <ul className="list-inline mx-auto skill-icon">{programming}</ul>
+          <div className="col-md-12 text-center">
+            <h2 className="subsection-title">
+              <span className="text-white">Industrial Skills</span>
+            </h2>
+            <ul className="list-inline mx-auto skill-icon">{technicalSkills}</ul>
+            <h2 className="subsection-title">
+              <span className="text-white">programming Skills</span>
+            </h2>
+            <ul className="list-inline mx-auto skill-icon">{programmingSkills}</ul>
           </div>
         </div>
       </section>
@@ -52,5 +54,5 @@ class Skills extends Component {
   }
 }
 
-export default Skills;
 
+export default Skills;
