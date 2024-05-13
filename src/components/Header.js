a2492fb91cd9102ea700ee3cf29a3d73bf9bcff7
Tatsuhiro Terada <tatsuhirotera@gmail.com>
2024-05-13 22:01:40 +0900

    Add Element

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import Switch from "react-switch";
 
 class Header extends Component {
   titles = [];
-
   constructor() {
     super();
     this.state = { checked: false };
@@ -19,28 +18,55 @@ class Header extends Component {
   setTheme() {
     var dataThemeAttribute = "data-theme";
     var body = document.body;
-    var newTheme =
-      body.getAttribute(dataThemeAttribute) === "dark" ? "light" : "dark";
+    var newTheme = body.getAttribute(dataThemeAttribute) === "dark" ? "light" : "dark";
     body.setAttribute(dataThemeAttribute, newTheme);
   }
 
   render() {
     if (this.props.sharedData) {
       var name = this.props.sharedData.name;
-      this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 1500 ] ).flat();
+      this.titles = this.props.sharedData.titles.map(x => [x.toUpperCase(), 1500]).flat();
     }
 
-    const HeaderTitleTypeAnimation = React.memo( () => {
-      return <Typical className="title-styles" steps={this.titles} loop={50} />
-    }, (props, prevProp) => true);
+    const HeaderTitleTypeAnimation = React.memo(
+      () => {
+        return <Typical className="title-styles" steps={this.titles} loop={50} />
+      },
+      (props, prevProp) => true
+    );
 
     return (
       <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
-        <div className="row aligner" style={{height: '100%'}}>
+        {/* <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+          <div className="container">
+            <a className="navbar-brand" href="#">Tatsuhiro Terada</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="#about">About</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#portfolio">Projects</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#skills">Skills</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#resume">Experience</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav> */}
+
+        <div className="row aligner" style={{ height: '100%' }}>
           <div className="col-md-12">
             <div>
               <span className="iconify header-icon" data-icon="fa:ship" data-inline="false"></span>
-              <br/>
+              <br />
               <h1 className="mb-0">
                 <Typical steps={[name]} wrapper="p" />
               </h1>
