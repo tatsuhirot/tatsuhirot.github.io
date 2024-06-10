8f57ad01ecd11e5caa52050826230a79c85e5ad4
Tatsuhiro Terada <tatsuhirotera@gmail.com>
2024-06-10 17:07:46 +0900

    Updates

---
+++ precache-manifest.b6d84603444aae118d66286788aea651.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5177a98284d8ca15e767ce809f1f154",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9e493274ede25b202ddf",
+    "url": "/react-portfolio/static/css/2.919df8cf.chunk.css"
+  },
+  {
+    "revision": "8834978627330ba2b183",
+    "url": "/react-portfolio/static/css/main.0ea3b32e.chunk.css"
+  },
+  {
+    "revision": "9e493274ede25b202ddf",
+    "url": "/react-portfolio/static/js/2.711b3c7d.chunk.js"
+  },
+  {
+    "revision": "c90d82b5d68344eb354f9a6ef56d4572",
+    "url": "/react-portfolio/static/js/2.711b3c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8834978627330ba2b183",
+    "url": "/react-portfolio/static/js/main.c5ec9eda.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce19ef00cc8948ceb21f305e85b75bab.js"
+  "/react-portfolio/precache-manifest.b6d84603444aae118d66286788aea651.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
