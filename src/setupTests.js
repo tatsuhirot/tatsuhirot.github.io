65db5d770f8f2032d75fe727608150028f8bdc4a
Tatsuhiro Terada <85213974+tatsuhirot@users.noreply.github.com>
2025-06-05 07:32:18 +0900

    Fix tests by polyfilling IntersectionObserver

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// Mock IntersectionObserver for tests
+class IntersectionObserver {
+  constructor() {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.IntersectionObserver = IntersectionObserver;
